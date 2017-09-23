fc209272d2f429330f6831eb50dc3bbc5e06523a
Bernardo Breder <bernardobreder@gmail.com>
2017-09-22 21:25:37 -0300

    .

---
+++ src/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  php
+//
+//  Created by Bernardo Breder on 29/09/15.
+//  Copyright © 2015 breder. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
